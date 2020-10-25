69f5572259bc0bef903e65121611597a5e2a940b
Antton Alberdi <anttonalberdi@Anttons-MacBook-Pro-2.local>
2020-10-25 15:23:26 +0100

    f

--- bin/bamse-filtering.R
+++ bin/bamse-filtering.R
@@ -1,14 +1,37 @@
 #2020/10/25 - BAMSE 1.0
+#use optparse for parsing variables
+#https://github.com/lachlandeer/snakemake-econ-r/blob/master/src/data-management/rename_variables.R
+
+# CLI parsing
+option_list = list(
+ make_option("-i1",type = "character",default = NULL,help = "stata dataset file name",metavar = "character"),
+ make_option("-i2"), type = "character", default = "NULL", help = "output file name [default = %default]", metavar = "character"),
+ make_option("-o1",type = "character",default = NULL,help = "stata dataset file name",metavar = "character"),
+ make_option("-o2"), type = "character", default = "NULL", help = "output file name [default = %default]", metavar = "character"),
+ make_option("--maxN",type = "character",default = NULL,help = "stata dataset file name",metavar = "character"),
+ make_option("--maxEE"), type = "character", default = "NULL", help = "output file name [default = %default]", metavar = "character"),
+ make_option("--truncQ",type = "character",default = NULL,help = "stata dataset file name",metavar = "character"),
+ make_option("--truncLen"), type = "character", default = "NULL", help = "output file name [default = %default]", metavar = "character"),
+
+);
+
+opt_parser = OptionParser(option_list = option_list);
+opt = parse_args(opt_parser);
+
+if (is.null(opt$data)){
+ print_help(opt_parser)
+ stop("At least one argument must be supplied (input file).n", call. = FALSE)
+}
 
 
-i1<-snakemake@input[["i1"]]
-i2<-snakemake@input[["i2"]]
-o1<-snakemake@input[["o1"]]
-o2<-snakemake@input[["o2"]]
-maxN<-snakemake@params[["maxN"]]
-maxEE<-snakemake@params[["maxEE"]]
-truncQ<-snakemake@params[["truncQ"]]
-truncLen<-snakemake@params[["truncLen"]]
+i1<-opt$i1
+i2<-opt$i2
+o1<-opt$01
+o2<-opt$02
+maxN<-opt$maxN
+maxEE<-opt$maxEE
+truncQ<-opt$truncQ
+truncLen<-opt$truncLen
 
 print(i1)
 print(i2)
