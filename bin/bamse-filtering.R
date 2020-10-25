7407e0c6d8fe80efe49ffac214c9fec9f7f44128
Antton Alberdi <anttonalberdi@Anttons-MacBook-Pro-2.local>
2020-10-25 13:09:19 +0100

    d

--- bin/bamse-filtering.R
+++ bin/bamse-filtering.R
@@ -10,5 +10,10 @@ maxEE<-Sys.getenv("maxEE")[6]
 truncQ<-Sys.getenv("truncQ")[7]
 truncLen<-Sys.getenv("truncLen")[8]
 
+print(i1)
+print(i2)
+print(o1)
+print(o1)
+
 library(dada2)
 filterAndTrim(i1, i2, o1, o2, maxN=0, maxEE=c(2,2), truncQ=2, rm.phix=TRUE, truncLen=c(100,100), compress=TRUE, multithread=TRUE)
