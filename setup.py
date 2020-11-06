3208696247c5b1310f6997965ab053e6d77a9c5e
Antton Alberdi <anttonalberdi@Anttons-MacBook-Pro-2.local>
2020-11-06 07:11:56 +0100

    Setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='BAMSE',
+      version='1.0',
+      description='Bacterial AMplicon SEquence data processing pipeline',
+      url='http://github.com/anttonalberdi/bamse',
+      author='Antton Alberdi',
+      author_email='antton.alberdi@sund.ku.dk',
+      license='MIT',
+      zip_safe=False)
