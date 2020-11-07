59b92f8c8ddb15bc87ca3f3e247eea99ef286205
Antton Alberdi <anttonalberdi@Anttons-MacBook-Pro-2.local>
2020-11-07 10:10:52 +0100

    Primertrim

---
+++ bin/bamse-primertrim.sh
@@ -0,0 +1,73 @@
+#2020/10/25 - BAMSE 1.0
+#Perl script taken from: http://userweb.eng.gla.ac.uk/umer.ijaz/bioinformatics/QC.html#perbase_quality_FASTQ.sh
+
+usage() { echo "Usage: $0 [-a read1.fastq] [-b read2.fastq] [-c read1.trimmed.fastq] [-d read2.trimmed.fastq] [-f primer1] [-r primer2]" 1>&2; exit 1; }
+
+while getopts ":a:b:c:d:f:r:" o; do
+    case "${o}" in
+
+        a)
+            a=${OPTARG}
+            ;;
+        b)
+            b=${OPTARG}
+            ;;
+        c)
+            c=${OPTARG}
+            ;;
+        d)
+            d=${OPTARG}
+            ;;
+				f)
+		        f=${OPTARG}
+		        ;;
+		    r)
+		        r=${OPTARG}
+		        ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+if [ -z "${a}" ] || [ -z "${b}" ] || [ -z "${c}" ] || [ -z "${d}" ] || [ -z "${f}" ] || [ -z "${r}" ]; then
+    usage
+fi
+
+read1=$a
+read2=$b
+filt1=$c
+filt2=$d
+primer1=$f
+primer2=$r
+
+#####
+# Trim primers
+#####
+
+cutadapt --pair-adapters -e 0.2 -g ^${primer1} -G ^${primer2} --discard-untrimmed -o ${filt1}_a -p ${filt2}_a ${read1} ${read2}
+cutadapt --pair-adapters -e 0.2 -g ^${primer2} -G ^${primer1} --discard-untrimmed -o ${filt1}_b -p ${filt2}_b ${read1} ${read2}
+
+#####
+# Check library type
+#####
+
+initial=$(cat ${read1} | wc -l)
+initial10=$(($initial / 10))
+dir5to3=$(cat ${filt1}_a | wc -l)
+dir3to5=$(cat ${filt1}_b | wc -l)
+
+if [ "$dir3to5" -gt "$initial10" ]; then
+#It is ligation-based library; so dir3to5 reads need to be flipped
+  cat ${filt1}_a ${filt2}_b > ${filt1}
+  cat ${filt2}_a ${filt1}_b > ${filt2}
+else
+#It is PCR-based library, so all reads are at the same direction
+  mv ${filt1}_a ${filt1}
+  mv ${filt2}_a ${filt2}
+fi
+
+#Remove temporary files
+rm ${filt1}_a ${filt1}_b ${filt2}_a ${filt2}_b
